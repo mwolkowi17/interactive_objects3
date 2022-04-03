e8edf41510c9e6954e7310dfff99568d3a89bb21
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2022-04-03 21:59:17 +0200

    rotation of main model added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import React, { Suspense} from 'react'
 import { Canvas} from '@react-three/fiber'
 
 import { OrbitControls, Html } from '@react-three/drei'
-
-
-//import { Model2 } from './Model2'
-//import { Model3 } from './Model3'
-//import { Model4 } from './Model4'
-//import { Model5 } from './Model5'
-//import { Model6 } from './Model6'
-//import { Model7 } from './Model7'
 import { Strips1 } from './Strips'
-//import { Shapes } from './Shapes'
-//import { Arrows1 } from './Arrows'
-//import { Text1 } from './Text'
-//import { Numbers } from './Numbers'
 import { ModelD } from './Diagram'
 
 
@@ -23,14 +11,12 @@ import { ModelD } from './Diagram'
 
 export default function App() {
   function Loader() {
-    //const { progress } = useProgress()
-    //return <Html center style={{ color: 'white' }}>{progress} % loaded</Html>
     return <Html center style={{ color: 'black' }}>loading...</Html>
   }
 
   return (
 
-    <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [-10, 6, 30] }}>
+    <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [-4, 6, 6] }}>
       <OrbitControls
         maxPolarAngle={Math.PI / 2}
         maxDistance={40}
@@ -41,7 +27,7 @@ export default function App() {
 
 
         <Strips1 position={[0, 0, 0]} />
-        <ModelD position={[0, 0, 0]}  />
+        <ModelD position={[0, -3, 0]} rotation={[0,Math.PI*3.6,0]} />
 {/*
         <Shapes position={[0, 0, 0]} />
         <Arrows1 position={[0, 0, 0]} />
